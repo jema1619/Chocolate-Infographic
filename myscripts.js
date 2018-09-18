fa6e44b58a325324b09d4149b5b8a89a2ac91255
jema1619 <jema1619@student.ju.se>
2018-09-18 16:27:37 +0200

    Infographic

---
+++ myscripts.js
@@ -0,0 +1,137 @@
+$(document).ready(function(){
+    $(".piece2").click(function(){
+        $(".front2").toggleClass("back2");
+    });
+});
+
+$(document).ready(function(){
+    $(".piece3").click(function(){
+        $(".front3").toggleClass("back3");
+    });
+});
+
+$(document).ready(function(){
+    $(".piece4").click(function(){
+        $(".front4").toggleClass("back4");
+    });
+});
+
+$(document).ready(function(){
+    $(".piece5").click(function(){
+        $(".front").toggleClass("back");
+    });
+});
+
+$(document).ready(function(){
+    $(".piece6").click(function(){
+        $(".front6").toggleClass("back6");
+    });
+});
+
+$(document).ready(function(){
+    $(".piece7").click(function(){
+        $(".front7").toggleClass("back7");
+    });
+});
+
+$(document).ready(function(){
+    $(".piece8").click(function(){
+        $(".front8").toggleClass("back8");
+    });
+});
+
+$(document).ready(function(){
+    $(".piece9").click(function(){
+        $(".front9").toggleClass("back9");
+    });
+});
+
+$(document).ready(function(){
+    $(".piece10").click(function(){
+        $(".front10").toggleClass("back10");
+    });
+});
+
+$(document).ready(function(){
+    $(".piece11").click(function(){
+        $(".front11").toggleClass("back11");
+    });
+});
+
+$(document).ready(function(){
+    $(".piece12").click(function(){
+        $(".front12").toggleClass("back12");
+    });
+});
+
+$(document).ready(function(){
+    $(".piece13").click(function(){
+        $(".front13").toggleClass("back13");
+    });
+});
+
+$(document).ready(function(){
+    $(".piece14").click(function(){
+        $(".front14").toggleClass("back14");
+    });
+});
+
+$(document).ready(function(){
+    $(".piece15").click(function(){
+        $(".front15").toggleClass("back15");
+    });
+});
+
+$(document).ready(function(){
+    $(".piece16").click(function(){
+        $(".front16").toggleClass("back16");
+    });
+});
+
+$(document).ready(function(){
+    $(".piece17").click(function(){
+        $(".front17").toggleClass("back17");
+    });
+});
+
+$(document).ready(function(){
+    $(".piece18").click(function(){
+        $(".front18").toggleClass("back18");
+    });
+});
+
+$(document).ready(function(){
+    $(".piece19").click(function(){
+        $(".front19").toggleClass("back19");
+    });
+});
+
+$(document).ready(function(){
+    $(".piece20").click(function(){
+        $(".front20").toggleClass("back20");
+    });
+});
+
+$(document).ready(function(){
+    $(".piece21").click(function(){
+        $(".front21").toggleClass("back21");
+    });
+});
+
+$(document).ready(function(){
+    $(".piece22").click(function(){
+        $(".front22").toggleClass("back22");
+    });
+});
+
+$(document).ready(function(){
+    $(".piece23").click(function(){
+        $(".front23").toggleClass("back23");
+    });
+});
+
+$(document).ready(function(){
+    $(".piece24").click(function(){
+        $(".front24").toggleClass("back24");
+    });
+});
